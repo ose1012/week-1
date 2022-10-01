944db98affbfe5815e65cd76b9c892e8f2d98c40
ose1012 <nnmmaz95@gmail.com>
2022-10-01 21:18:02 +0900

    폼 커밋

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,19 +4,9 @@ import List from '../list/List';
 import TodoList from '../pages/TodoList';
 import './style.css';
 function Form() {
-  const [inputs, setInputs] = useState({
-    title: '',
-    body: ''
-  });
-  const { title, body } = inputs;
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    const {value, name} = event.target;
-    setInputs({
-      ...inputs, [name] : value
-    });
-  };
+  const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if( toDo === ""){
@@ -29,9 +19,9 @@ function Form() {
       <><form className="add-form" onSubmit={onSubmit}>
         <div className="input-group">
           <label className="form-label">제목</label>
-          <input type="text" name="title" className="add-input input-body" onChange={onChange} value={toDo.title} />
+          <input type="text" name="title" className="add-input input-body" />
           <label className="form-label">내용</label>
-          <input type="text" name="body" className="add-input" onChange={onChange} value={toDo.body}/>
+          <input type="text" name="body" className="add-input" onChange={onChange} value={toDo} />
         </div>
         <button className="add-button">추가하기</button>
       </form>
@@ -39,6 +29,5 @@ function Form() {
       </>
     );
   }
-  
 
 export default Form;
